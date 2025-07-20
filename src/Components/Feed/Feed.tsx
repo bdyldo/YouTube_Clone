deaf401bd87a63d2b06b1f8548d6278241e6c7bf
bdyldo <dyl123455@outlook.com>
2025-07-20 19:59:02 -0400

    added PlayVideo

--- src/Components/Feed/Feed.tsx
+++ src/Components/Feed/Feed.tsx
@@ -1,4 +1,4 @@
-import React, {Link} from 'react'
+import React from 'react'
 import './Feed.css'
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
@@ -8,6 +8,7 @@ import thumbnail5 from '../../assets/thumbnail5.png'
 import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
+import { Link } from 'react-router-dom'
 
 const Feed = () => {
   return (
--- src/pages/Home/Video/Video.tsx
+++ src/pages/Home/Video/Video.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './video.css'
+import PlayVideo from '../../../Components/PlayVideo/PlayVideo'
 
 const video = () => {
   return (
     <div className='play-container'>
-      
+      <PlayVideo/>
     </div>
   )
 }

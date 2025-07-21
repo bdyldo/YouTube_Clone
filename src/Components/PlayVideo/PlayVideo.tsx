86b96c3966a24af555b23c95e15e1342c2d1cb72
bdyldo <dyl123455@outlook.com>
2025-07-21 18:19:38 -0400

    Finished PlayVideo Page and its CSS

--- src/Components/PlayVideo/PlayVideo.tsx
+++ src/Components/PlayVideo/PlayVideo.tsx
@@ -6,12 +6,49 @@ import dislike from '../../assets/dislike.png'
 import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
-import user_profile from '../../assets/user_profile.png'
+import user_profile from '../../assets/user_profile.jpg'
 
 const PlayVideo = () => {
   return (
     <div className = 'play-video'>
-        <video src = {video1}></video>
+        <video src = {video1} controls autoPlay muted></video>
+        <h3>Some Title</h3>
+        <div className="play-video-info">
+            <p>1000 Views &bull; 2 days ago</p>
+            <div>
+                <span><img src = {like} alt = ""></img>125</span>
+                <span><img src = {dislike} alt = ""></img>2</span>
+                <span><img src = {share} alt = ""></img>Share</span>
+                <span><img src = {save} alt = ""></img>Save</span>
+            </div>
+        </div>
+        <hr/>
+        <div className="publisher">
+            <img src={jack} alt="" />
+            <div className="publisher-info">
+                <p>Channel Info</p>
+                <span>100k Subscribers</span>
+            </div>
+            <button className='subscribe-btn'>Subscribe</button>
+        </div>
+        <div className="vid-description">
+            <p>Some description</p>
+            <p>Some other descriptions</p>
+            <hr/>
+            <h4>100 Comments</h4>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Some commenter <span>1 day ago</span></h3>
+                    <p>Some comment</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }

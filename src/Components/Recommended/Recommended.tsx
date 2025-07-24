d7849f73cb41d272ee8d7293b24288b05e4e07b1
bdyldo <dyl123455@outlook.com>
2025-07-23 22:25:40 -0400

    Finished rendering recommended vids

--- src/Components/Recommended/Recommended.tsx
+++ src/Components/Recommended/Recommended.tsx
@@ -1,83 +1,41 @@
-import React from 'react'
-import './Recommended.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
+import React, { useState, useEffect } from "react";
+import "./Recommended.css";
+import { API_KEY } from "../../data";
+import { value_converter } from "../../data.ts";
 
-const Recommended = () => {
+const Recommended = ({ categoryId }: any) => {
+  const [apiData, setApiData] = useState([]);
+
+  const fetchData = async () => {
+    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+    await fetch(relatedVideo_url)
+      .then((response) => response.json())
+      .then((data) => {
+        setApiData(data.items);
+      })
+      .catch((error) => console.error("Error fetching related videos:", error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className='recommended'>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="" />
+    <div className="recommended">
+      {apiData.map((item: any, index) => {
+        return (
+          <div key = {index} className="side-video-list">
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
             <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
+              <h4>{item.snippet.title}</h4>
+              <p>{item.snippet.channelTitle}</p>
+              <p>{value_converter(item.statistics.viewCount)}</p>
             </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail2} alt="" />
-            <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail3} alt="" />
-            <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail4} alt="" />
-            <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail5} alt="" />
-            <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail6} alt="" />
-            <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail7} alt="" />
-            <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail8} alt="" />
-            <div className="vid-info">
-                <h4>Title 1</h4>
-                <p>Channel Name</p>
-                <p>100k Views</p>
-            </div>
-        </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Recommended
+export default Recommended;
--- src/pages/Home/Video/Video.tsx
+++ src/pages/Home/Video/Video.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import './video.css'
-import PlayVideo from '../../../Components/PlayVideo/PlayVideo'
-import Recommended from '../../../Components/Recommended/Recommended'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import "./video.css";
+import PlayVideo from "../../../Components/PlayVideo/PlayVideo";
+import Recommended from "../../../Components/Recommended/Recommended";
+import { useParams } from "react-router-dom";
 
 const Video = () => {
-
-  const {videoId, categoryId} = useParams<{videoId: string, categoryId: string}>();
+  const { videoId, categoryId } = useParams<{
+    videoId: string;
+    categoryId: string;
+  }>();
   console.log("Loaded Video", categoryId, videoId);
 
   return (
-    <div className='play-container'>
-      <PlayVideo videoId = {videoId}/>
-      <Recommended/>
+    <div className="play-container">
+      <PlayVideo videoId={videoId} />
+      <Recommended categoryId={categoryId} />
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
